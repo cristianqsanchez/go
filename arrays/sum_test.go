c4a560f8b0e34ebb671ec0ae8383858be0a78c3c
cristianqsanchez <cristianqsdev@gmail.com>
2024-06-28 00:16:08 -0500

    feat: add all slices sum

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,9 @@
 package arrays
 
-import "testing"
+import (
+	"testing"
+  "slices"
+)
 
 func TestSum(t *testing.T) {
   t.Run("collection of 5 numbers", func(t *testing.T) {
@@ -13,4 +16,13 @@ func TestSum(t *testing.T) {
       t.Errorf("\033[33;1m got %d want %d, given %v \033[0m", got, want, numbers)
     }
   })
+}
+
+func TestSumAll(t *testing.T) {
+  got := SumAll([]int{1, 2}, []int{0, 9})
+  want := []int{3, 9}
+
+  if !slices.Equal(got, want) {
+    t.Errorf("got %v want %v", got, want)
+  }
 }
